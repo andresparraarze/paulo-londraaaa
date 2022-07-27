0f889df8043153a993bb34c60d1f7ed873b69790
Parra <andresparraarze@gmail.com>
2022-07-27 00:44:46 -0400

    thought route file setup

---
+++ routes/api/thought-route.js
@@ -0,0 +1,12 @@
+const router = require('express').Router();
+const {getThoughts, getSingleThought, createThought, updateThought, deleteThought} = require('../../controllers/thought-controller');
+
+router.route('/').get(getThoughts).post(createThought);
+
+router
+  .route('/:thoughtId')
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+module.exports = router;
